29544b1e5c75d71a7b321b0de63c9a66498fa796
visteen <visteen192@gmail.com>
2023-08-03 21:25:29 +0000

    main page added

--- frontend/y_check/src/App.js
+++ frontend/y_check/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Home from './components/home/Home';
 import Nav from './components/nav/Nav';
+import MainPage from './pages/main/MainPage';
 function App() {
   return (
     <div className="App">
-      <Nav/>
+      <MainPage/>
     </div>
   );
 }
---
+++ frontend/y_check/src/pages/main/MainPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './main_page.scss'
+function MainPage() {
+  return (
+    <div className='main'>
+    </div>
+  )
+}
+
+export default MainPage
