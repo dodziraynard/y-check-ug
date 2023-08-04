bb3a5f5203548e81406cd5980f8c82b67c06ba11
visteen <visteen192@gmail.com>
2023-08-04 14:34:18 +0000

    chart added

--- frontend/y_check/src/components/chart/Chart.jsx
+++ frontend/y_check/src/components/chart/Chart.jsx
@@ -1,13 +1,45 @@
 import React from 'react'
 import './chart.scss'
+import {Chart as ChartJs,Tooltip,Title,ArcElement,Legend} from 'chart.js'
+import { Doughnut } from 'react-chartjs-2';
+ChartJs.register(
+    Tooltip,Title,ArcElement,Legend
+)
 
+const chartOptions = {
+    plugins: {
+      legend: {
+        position: 'right', // Place the legend on the left
+      },
+    },
+  };
+
+const data = {
+    labels: [
+      'Primary',
+      'secondary',
+      'community'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 100, 50],
+      backgroundColor: [
+        'rgb(54,162,235)',
+        'rgb(255, 99, 132)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
 function Chart() {
   return (
-    <section>
-        <div>
-            <h1>Louis Seyram</h1>
-        </div>
-    </section>
+    <section className='section'>
+    <div className='chart-container'>
+      <div className='chart'>
+        <Doughnut data={data}  options={chartOptions}/>
+      </div>
+    </div>
+  </section>
   )
 }
 
