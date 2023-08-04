b728f29cc20a0f11d8388e9161b6da8cf8a1ca07
visteen <visteen192@gmail.com>
2023-08-04 11:47:26 +0000

    card.scss added

--- frontend/y_check/src/components/card/Card.jsx
+++ frontend/y_check/src/components/card/Card.jsx
@@ -1,45 +1,46 @@
 import React from 'react'
 import './card.scss'
 import Icon from '@mdi/react';
-import { mdiAccountInjuryOutline } from '@mdi/js';
+import { mdiAccountInjuryOutline,mdiDoctor,mdiAccountSupervisor,mdiCalendarAlertOutline } from '@mdi/js';
 function Card() {
   return (
     <section>
-        <div className='analytic'>
-            <div className="content">
-                <h2>147</h2>
-                <h5>patients</h5>
-            </div>
+        <div className='card'>
             <div className="logo">
-            <Icon path={mdiAccountInjuryOutline} size={1} />
+            <Icon path={mdiAccountInjuryOutline} size={3} />
+            </div>
+            <div className="content">
+                <h2>250</h2>
+                <h5>Total Patients</h5>
             </div>
         </div>
-        <div className='analytic'>
-            <div className="content">
-                <h2>147</h2>
-                <h5>patients</h5>
-            </div>
+        <div className='card'>
             <div className="logo">
-            <Icon path={mdiAccountInjuryOutline} size={1} />
+            <Icon path={mdiCalendarAlertOutline} size={3} />
+            </div>
+            <div className="content">
+                <h2>90</h2>
+                <h5>Appointment</h5>
             </div>
         </div>
-        <div className='analytic'>
-            <div className="content">
-                <h2>147</h2>
-                <h5>patients</h5>
-            </div>
+        <div className='card'>
             <div className="logo">
-            <Icon path={mdiAccountInjuryOutline} size={1} />
+            <Icon path={mdiDoctor} size={3} />
+            </div>
+            <div className="content">
+                <h2>167</h2>
+                <h5>Total Users</h5>
             </div>
         </div>
-        <div className='analytic'>
-            <div className="content">
-                <h2>147</h2>
-                <h5>patients</h5>
-            </div>
+        <div className='card'>
             <div className="logo">
-            <Icon path={mdiAccountInjuryOutline} size={1} />
+            <Icon path={mdiAccountSupervisor} size={3} />
             </div>
+            <div className="content">
+                <h2>13</h2>
+                <h5>Groups</h5>
+            </div>
+            
         </div>
     </section>
   )
