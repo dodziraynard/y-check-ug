2f1bfea638ae5460e301c9f10ee050fd36a60b5c
visteen <visteen192@gmail.com>
2023-08-15 14:57:27 +0000

    pagination added

--- frontend/y_check/src/components/patientDatail/Detail.jsx
+++ frontend/y_check/src/components/patientDatail/Detail.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
 import person from '../../images/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg'
 import Icon from '@mdi/react';
 import { mdiTrashCanOutline,mdiPencilOutline,mdiChevronLeft,mdiChevronRight} from '@mdi/js';
 
 const Detail = () => {
+    const propertiesPerPage = 8; // Number of items per page
+    
+
+    const initialData = {
+        PIP: 'yc100w',
+        Name: 'louis',
+        Sex: 'male',
+        Age: '17',
+        Type: 'primary',
+        Town: 'madina',
+        School: 'ug-basic',
+        Visit: 'initial',
+        Mother: 'happy',
+        Father: 'Samuel',
+        Dob: '25/08/2005',
+        I_live_with: 'parents',
+        Height: '4',
+        Weight: '120',
+        Bmi: '60',
+        Hvi: 'positive',
+        Cell: 'no',
+    };
+      
+    const propertyKeys = Object.keys(initialData);
+    const totalPages = Math.ceil(propertyKeys.length / propertiesPerPage);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const startIndex = (currentPage - 1) * propertiesPerPage;
+    const endIndex = startIndex + propertiesPerPage;
+    const currentProperties = propertyKeys.slice(startIndex, endIndex);
+
+   
+    const handlePageChange = pageNumber => {
+        if (pageNumber >= 1 && pageNumber <= totalPages) {
+          setCurrentPage(pageNumber);
+        }
+    };
+    
+    const handleNextPage = () => {
+        handlePageChange(currentPage + 1);
+    };
+    
+    const handlePreviousPage = () => {
+        handlePageChange(currentPage - 1);
+    };
+    
   return (
     <div className="patient-detail-main">
             <div className="left-side">
@@ -28,46 +74,26 @@ const Detail = () => {
                 </div>
                 <hr className='hr-divider-top'/>
                 <div className='information'>
-                    <div className='info-details'>
-                        <h4>Name</h4>
-                        <span>Louis Seyram</span>
-                    </div>
-                    <div className='info-details'>
-                        <h4>PIP</h4>
-                        <span>YC1001</span>
-                    </div>
-                    <div className='info-details'>
-                        <h4>Sex</h4>
-                        <span>Male</span>
-                    </div>
-                    <div className='info-details'>
-                        <h4>Date of Birth</h4>
-                        <span>25/08/2007</span>
-                    </div>
-                    <div className='info-details'>
-                        <h4>Adolescent Type</h4>
-                        <span>Primary</span>
-                    </div>
-                    <div className='info-details'>
-                        <h4>Visit</h4>
-                        <span>Initial</span>
-                    </div>
-                    <div className='info-details'>
-                        <h4>Check Up Location</h4>
-                        <span>Madina</span>
-                    </div>
-                    <div className='info-details'>
-                        <h4>School</h4>
-                        <span>UG Basic School</span>
-                    </div>
+                {currentProperties.map(property => (
+                <div key={property} className='info-details'>
+                    <h4>{property}</h4>
+                    <span>{initialData[property]}</span>
+                </div>
+                ))}
                 </div>
                 <div className='next'>
-                    <h5 className="section-heading">Showing Section 1 of 12</h5>
+                    <h5 className="section-heading">Showing Section {currentPage} of {totalPages}</h5>
                     <hr className='hr-divider'/>
                     <div className="button-container">
+                    <button className='pre' onClick={handlePreviousPage}>
                         <Icon className='chevron' path={mdiChevronLeft} size={1} />
-                        <button>1</button>
+                    </button>
+                        <button className='next-button'>
+                        {currentPage}
+                        </button>
+                    <button className='pre' onClick={handleNextPage}>
                         <Icon className='chevron' path={mdiChevronRight} size={1} />
+                    </button>
                     </div>
                     <hr className='hr-divider'/>
                 </div>
