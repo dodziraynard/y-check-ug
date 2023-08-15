3eacdbed8a1ee3dbcdf611bd85079f080e22ddbe
visteen <visteen192@gmail.com>
2023-08-15 18:18:02 +0000

    login page added

--- frontend/y_check/src/App.js
+++ frontend/y_check/src/App.js
@@ -3,13 +3,14 @@ import Home from './components/home/Home';
 import MainPage from './pages/main/MainPage';
 import PatientPage from './pages/PatientPage/PatientPage';
 import PatientViewPage from './pages/PatientViewPage/PatientViewPage';
+import LoginPage from './pages/loginPage/LoginPage';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path='/'element={<Home/>}/>
+      <Route path='/'element={<LoginPage/>}/>
       <Route path='/dashboard'element={<MainPage/>}/>
       <Route path='/patients' element={<PatientPage/>}/>
       <Route path='/patient-view' element={<PatientViewPage/>}/>
---
+++ frontend/y_check/src/pages/loginPage/LoginPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './login.scss'
+import ug_logo from '../../images/UoG_CoA_2017.svg.png' ;
+import Icon from '@mdi/react';
+import { mdiAccount,mdiEyeOutline } from '@mdi/js';
+const LoginPage = () => {
+  return (
+    <div className='login'>
+        <div className='login-container'>
+            <div className='login-image'>
+                <img src={ug_logo} alt="Logo" />
+            </div>
+            <div className="login-title">
+                <h2>Login</h2>
+            </div>
+            <div className="login-form">
+                <span>Please input the correct credential</span>
+                <div className="input-with-icon">
+                    <input type="text" placeholder='Staff ID:'/>
+                    <Icon className='login-icon' path={mdiAccount} size={1} />
+                </div>
+                <div className="input-with-icon">
+                    <input type="password" placeholder='Password:'/>
+                    <Icon className='login-icon' path={mdiEyeOutline} size={1} />
+                </div>
+                <span>Forget password? <span style={{color:'#B5965C',cursor:'pointer'}}>reset</span></span>
+                <button>LOGIN</button>
+                <span>Don't have an account?<span style={{color:'#B5965C',cursor:'pointer'}}> sign up</span></span>
+            </div>
+        </div>
+    </div>
+
+  )
+}
+
+export default LoginPage
