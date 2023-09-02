4514781201f0a19ac2f41d3cb7bc502748827970
Raynard Dodzi Helegah <70073581+hrdpayhub@users.noreply.github.com>
2023-09-02 07:44:03 +0100

    refactored backend code

--- backend/manage.py
+++ backend/manage.py
@@ -9,10 +9,7 @@ from ycheck.settings import base
 def main():
     """Run administrative tasks."""
 
-    if base.DEBUG:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ycheck.settings.local')
-    else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ycheck.settings.production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ycheck.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
