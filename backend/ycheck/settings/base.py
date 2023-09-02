889c10d3e8ca40fc2856160d3ce6c38aa30fe5aa
Raynard Dodzi Helegah <70073581+hrdpayhub@users.noreply.github.com>
2023-09-02 07:38:43 +0100

    refactored backend code

--- backend/ycheck/settings/base.py
+++ backend/ycheck/settings/base.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,11 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config(
+SECRET_KEY = os.environ(
     'SECRET_KEY', default='qd#3^56(!u=x*&ij8f3x$&iuvzan%#6@ge)ssbqq_87ahoxifp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = int(os.environ.get("DEBUG", default=1))
+
 
 ALLOWED_HOSTS = ['*']
 
@@ -154,3 +155,24 @@ MEDIA_ROOT = BASE_DIR / "assets"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://*.127.0.0.1',
+    'http://localhost',
+]
+
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+CELERY_BROKER_URL = os.environ.get("REDIS_URL", "redis://localhost:6379")
+CELERY_RESULT_BACKEND = os.environ.get("REDIS_URL", "redis://localhost:6379")
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_IMPORTS = [
+]
+
+REDIS_HOST = os.environ.get(
+    "REDIS_URL", "redis://localhost:6379").split("//")[1].split(":")[0]
+REDIS_PORT = os.environ.get(
+    "REDIS_URL", "redis://localhost:6379").split("//")[1].split(":")[1]
