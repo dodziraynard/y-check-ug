817ec3470fdc90849f33eed8c29181a81e97ffcb
Raynard Dodzi Helegah <70073581+hrdpayhub@users.noreply.github.com>
2023-09-02 07:42:00 +0100

    refactored backend code

--- backend/ycheck/settings/base.py
+++ backend/ycheck/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ(
+SECRET_KEY = os.environ.get(
     'SECRET_KEY', default='qd#3^56(!u=x*&ij8f3x$&iuvzan%#6@ge)ssbqq_87ahoxifp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
