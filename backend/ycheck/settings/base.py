994f3412ff646b473f2b4ec58c6cd5f1dd67f746
Raynard Dodzi Helegah <70073581+hrdpayhub@users.noreply.github.com>
2023-09-02 08:18:40 +0100

    refactored backend code

--- backend/ycheck/settings/base.py
+++ backend/ycheck/settings/base.py
@@ -64,10 +64,10 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'django.middleware.security.SecuritzyMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
