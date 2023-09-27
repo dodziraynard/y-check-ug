c536d7ac350de529d7941ec675a1cb6c1d19d302
Raynard Dodzi Helegah <70073581+hrdpayhub@users.noreply.github.com>
2023-09-27 13:26:27 +0100

    remove old frontend app

--- frontend/y_check/src/constants/Host.js
+++
@@ -1,6 +0,0 @@
-// export const  BASE_URL = 'http://127.0.0.1:8000'
-
-export let BASE_URL = 'http://127.0.0.1:8000';
-if (process.env.NODE_ENV === 'production') {
-    BASE_URL = 'http://208.64.33.75:8000'
-}
--- frontend/y_check/src/permissions/Permissions.js
+++
@@ -1,6 +0,0 @@
-const Permissions = {
-    MANAGE_SETUP: "manage_setup",
-    VIEW_DASHBOARD: "view_dashboard",
-}
-
-export default Permissions;
