6737ef47b09d7ad5b970b7d6b8a660fc7740163b
Raynard Dodzi Helegah <70073581+hrdpayhub@users.noreply.github.com>
2023-10-22 16:31:29 +0100

    show details on page

--- backend/dashboard/admin.py
+++ backend/dashboard/admin.py
@@ -2,10 +2,28 @@ from django.contrib import admin
 
 from dashboard.models import *
 
+
+class OptionInline(admin.TabularInline):
+    model = Option
+
+
+class PreviousResponseRequirementInline(admin.TabularInline):
+    fk_name = "requirement_for"
+    model = PreviousResponseRequirement
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['text__icontains', 'number', 'question_id']
+    inlines = [
+        OptionInline,
+        PreviousResponseRequirementInline,
+    ]
+
+
 admin.site.register(CheckupLocation)
 admin.site.register(Section)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(PreviousResponseRequirement)
-admin.site.register(Question)
 admin.site.register(Option)
 admin.site.register(AdolescentResponse)
 admin.site.register(Adolescent)
