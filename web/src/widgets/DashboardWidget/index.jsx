6c03ee43f5b5d04e86e435d73a89b62bcff1cfd8
visteen <visteen192@gmail.com>
2023-11-19 11:23:17 +0000

    pie and bar chart component added

--- web/src/widgets/DashboardWidget/index.jsx
+++ web/src/widgets/DashboardWidget/index.jsx
@@ -1,12 +1,22 @@
 import BreadCrumb from '../../components/BreadCrumb';
 import './style.scss';
+import PieChart from '../../components/graph/PieChart';
+import BarChart from '../../components/graph/BarChart';
 
 function DashboardWidget() {
+
 
     return (
         <div className="dashboard-widget">
             <BreadCrumb />
-            DashboardWidget
+            <div className="row">
+                <div className="col-md-6">
+                    <PieChart/>
+                </div>
+                <div className="col-md-6">
+                    <BarChart/>
+                </div>
+            </div>
         </div>
     );
 }
