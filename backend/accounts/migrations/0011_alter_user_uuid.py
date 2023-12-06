e8665f49e2f17828ecaf2be542389b87da7eb4d8
Raynard Dodzi Helegah <70073581+hrdpayhub@users.noreply.github.com>
2023-12-06 03:45:42 +0000

    fix migrations

--- backend/accounts/migrations/0011_alter_user_uuid.py
+++ backend/accounts/migrations/0011_alter_user_uuid.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='user',
             name='uuid',
-            field=models.UUIDField(db_index=True, default=uuid.uuid4, unique=True),
+            field=models.UUIDField(db_index=True, default=uuid.uuid4),
         ),
     ]
