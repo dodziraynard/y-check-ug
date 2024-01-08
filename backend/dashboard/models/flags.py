4e09476fa2f41d36dd21a5b81b653fa0a1db54ad
Raynard Dodzi Helegah <70073581+hrdpayhub@users.noreply.github.com>
2024-01-08 01:51:36 +0000

    fix: make 3 max sd

--- backend/dashboard/models/flags.py
+++ backend/dashboard/models/flags.py
@@ -100,7 +100,7 @@ class FlagLabel(UpstreamSyncBaseModel):
         return self.name
 
     def get_flag_color(self, adolescent):
-        colors = self.colors.all().order_by("is_fallback")
+        colors = self.colors.all().order_by("is_fallback", "-color_name")
         fallback_color = None
         for color in colors:
             if color.is_fallback:
