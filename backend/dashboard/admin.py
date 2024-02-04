4ae2420dff5b96990284364d5ac3c6d541ca63c5
Raynard Dodzi Helegah <70073581+hrdpayhub@users.noreply.github.com>
2024-02-04 00:45:33 +0000

    search questions with ids

--- backend/dashboard/admin.py
+++ backend/dashboard/admin.py
@@ -22,7 +22,7 @@ class FlagConditionInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    search_fields = ['text__icontains', 'number', 'question_id']
+    search_fields = ["id", 'text__icontains', 'number', 'question_id']
     ordering = ['number']
     inlines = [
         OptionInline,
