da7c8216becebd50933ab7206f0e196af7404743
dodziraynard <dodzireynard@gmail.com>
2024-03-23 21:40:54 +0000

    Get base url dynamically

--- web/src/utils/constants.js
+++ web/src/utils/constants.js
@@ -5,9 +5,14 @@ const base_api = localStorage.getItem("base_api");
 if (base_api?.includes("http") == true) {
     BASE_API_URI = base_api
 } else if (process.env.NODE_ENV === 'production') {
-    BASE_API_URI = 'http://167.71.140.136:8000/api'
-}
+    const url = window.location.href;
 
+    if (url.search("://test.") >= 0) {
+        BASE_API_URI = 'https://test-api.ycheckgh.com/api'
+    } else {
+        BASE_API_URI = 'https://api.ycheckgh.com/api'
+    }
+}
 
 export const FLAG_GREY = "#808080"
 export const FLAG_RED = "#ff0000"
