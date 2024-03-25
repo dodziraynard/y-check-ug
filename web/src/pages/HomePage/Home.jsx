4b78b877b69e9d6810432961205999fe6eb4f5c9
visteen <visteen192@gmail.com>
2024-03-25 16:52:15 +0000

    about changed from row to columns

--- web/src/pages/HomePage/Home.jsx
+++ web/src/pages/HomePage/Home.jsx
@@ -143,26 +143,28 @@ const Home = () => {
                     <div class="features-wrap">
                         <div class="feature is-revealing">
                             <div class="feature-inner">
-                                <div class="feature-icon">
-                                    <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
-                                        <defs>
-                                            <linearGradient x1="0%" y1="100%" x2="50%" y2="0%" id="feature-1-a">
-                                                <stop stop-color="rgb(41, 110, 229)" stop-opacity=".8" offset="0%"/>
-                                                <stop stop-color="rgb(41, 110, 229)" stop-opacity=".16" offset="100%"/>
-                                            </linearGradient>
-                                            <linearGradient x1="50%" y1="100%" x2="50%" y2="0%" id="feature-1-b">
-                                                <stop stop-color="#b79a64" offset="0%"/>
-                                                <stop stop-color="#ffbf0d" stop-opacity=".798" offset="49.935%"/>
-                                                <stop stop-color="#b79a64" stop-opacity="0" offset="100%"/>
-                                            </linearGradient>
-                                        </defs>
-                                        <g fill="none" fill-rule="evenodd">
-                                            <path d="M24 48H0V24C0 10.745 10.745 0 24 0h24v24c0 13.255-10.745 24-24 24" fill="url(#feature-1-a)"/>
-                                            <path d="M40 64H16V40c0-13.255 10.745-24 24-24h24v24c0 13.255-10.745 24-24 24" fill="url(#feature-1-b)"/>
-                                        </g>
-                                    </svg>
+                                <div className="row-icon">
+                                    <div class="feature-icon">
+                                        <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
+                                            <defs>
+                                                <linearGradient x1="0%" y1="100%" x2="50%" y2="0%" id="feature-1-a">
+                                                    <stop stop-color="rgb(41, 110, 229)" stop-opacity=".8" offset="0%"/>
+                                                    <stop stop-color="rgb(41, 110, 229)" stop-opacity=".16" offset="100%"/>
+                                                </linearGradient>
+                                                <linearGradient x1="50%" y1="100%" x2="50%" y2="0%" id="feature-1-b">
+                                                    <stop stop-color="#b79a64" offset="0%"/>
+                                                    <stop stop-color="#ffbf0d" stop-opacity=".798" offset="49.935%"/>
+                                                    <stop stop-color="#b79a64" stop-opacity="0" offset="100%"/>
+                                                </linearGradient>
+                                            </defs>
+                                            <g fill="none" fill-rule="evenodd">
+                                                <path d="M24 48H0V24C0 10.745 10.745 0 24 0h24v24c0 13.255-10.745 24-24 24" fill="url(#feature-1-a)"/>
+                                                <path d="M40 64H16V40c0-13.255 10.745-24 24-24h24v24c0 13.255-10.745 24-24 24" fill="url(#feature-1-b)"/>
+                                            </g>
+                                        </svg>
+                                    </div>
+                                    <h3 class="feature-title mt-24">What is Y-Check</h3>
                                 </div>
-                                <h3 class="feature-title mt-24">What is Y-Check</h3>
                                 <p class="text-sm mb-0">
                                 Y-Check is a routine health check-ups programme developed as part of the WHO-coordinated Y-Check Research Programme.
                                 It aims to provide systematic health check-ups for adolescents in Ghana, Tanzania, and Zimbabwe.
@@ -175,26 +177,28 @@ const Home = () => {
                         </div>
                         <div class="feature is-revealing">
                             <div class="feature-inner">
-                                <div class="feature-icon">
-                                    <svg width="68" height="64" xmlns="http://www.w3.org/2000/svg">
-                                        <defs>
-                                            <linearGradient x1="0%" y1="100%" x2="50%" y2="0%" id="feature-2-a">
-                                                <stop stop-color="rgb(41, 110, 229)" stop-opacity=".8" offset="0%"/>
-                                                <stop stop-color="rgb(41, 110, 229)" stop-opacity=".16" offset="100%"/>
-                                            </linearGradient>
-                                            <linearGradient x1="50%" y1="100%" x2="50%" y2="0%" id="feature-2-b">
-                                                <stop stop-color="#b79a64" offset="0%"/>
-                                                <stop stop-color="#ffbf0d" stop-opacity=".798" offset="49.935%"/>
-                                                <stop stop-color="#b79a64" stop-opacity="0" offset="100%"/>
-                                            </linearGradient>
-                                        </defs>
-                                        <g fill="none" fill-rule="evenodd">
-                                            <path d="M9.941 63.941v-24c0-13.255 10.745-24 24-24h24v24c0 13.255-10.745 24-24 24h-24z" fill="url(#feature-2-a)" transform="rotate(45 33.941 39.941)"/>
-                                            <path d="M16 0v24c0 13.255 10.745 24 24 24h24V24C64 10.745 53.255 0 40 0H16z" fill="url(#feature-2-b)"/>
-                                        </g>
-                                    </svg>
+                                <div className="row-icon">
+                                    <div class="feature-icon">
+                                        <svg width="68" height="64" xmlns="http://www.w3.org/2000/svg">
+                                            <defs>
+                                                <linearGradient x1="0%" y1="100%" x2="50%" y2="0%" id="feature-2-a">
+                                                    <stop stop-color="rgb(41, 110, 229)" stop-opacity=".8" offset="0%"/>
+                                                    <stop stop-color="rgb(41, 110, 229)" stop-opacity=".16" offset="100%"/>
+                                                </linearGradient>
+                                                <linearGradient x1="50%" y1="100%" x2="50%" y2="0%" id="feature-2-b">
+                                                    <stop stop-color="#b79a64" offset="0%"/>
+                                                    <stop stop-color="#ffbf0d" stop-opacity=".798" offset="49.935%"/>
+                                                    <stop stop-color="#b79a64" stop-opacity="0" offset="100%"/>
+                                                </linearGradient>
+                                            </defs>
+                                            <g fill="none" fill-rule="evenodd">
+                                                <path d="M9.941 63.941v-24c0-13.255 10.745-24 24-24h24v24c0 13.255-10.745 24-24 24h-24z" fill="url(#feature-2-a)" transform="rotate(45 33.941 39.941)"/>
+                                                <path d="M16 0v24c0 13.255 10.745 24 24 24h24V24C64 10.745 53.255 0 40 0H16z" fill="url(#feature-2-b)"/>
+                                            </g>
+                                        </svg>
+                                    </div>
+                                    <h3 class="feature-title mt-24">Goal</h3>
                                 </div>
-                                <h3 class="feature-title mt-24">Goal</h3>
                                 <p class="text-sm mb-0" >
                                     The goal is to identify key contextual information on adolescent health,
                                     determine specific components of the health and well-being check-up intervention
@@ -206,31 +210,33 @@ const Home = () => {
                         </div>
                         <div class="feature is-revealing">
                             <div class="feature-inner">
-                                <div class="feature-icon">
-                                    <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
-                                        <defs>
-                                            <linearGradient x1="50%" y1="100%" x2="50%" y2="43.901%" id="feature-3-a">
-                                                <stop stop-color="rgb(41, 110, 229)" stop-opacity=".798" offset="0%"/>
-                                                <stop stop-color="rgb(41, 110, 229)" stop-opacity="0" offset="100%"/>
-                                            </linearGradient>
-                                            <linearGradient x1="58.893%" y1="100%" x2="58.893%" y2="18.531%" id="feature-3-b">
-                                                <stop stop-color="rgb(41, 110, 229)" stop-opacity=".8" offset="0%"/>
-                                                <stop stop-color="rgb(41, 110, 229)" stop-opacity="0" offset="100%"/>
-                                            </linearGradient>
-                                            <linearGradient x1="50%" y1="100%" x2="50%" y2="0%" id="feature-3-c">
-                                                <stop stop-color="#b79a64" offset="0%"/>
-                                                <stop stop-color="#ffbf0d" stop-opacity=".798" offset="49.935%"/>
-                                                <stop stop-color="#b79a64" stop-opacity="0" offset="100%"/>
-                                            </linearGradient>
-                                        </defs>
-                                        <g fill="none" fill-rule="evenodd">
-                                            <path fill="url(#feature-3-a)" opacity=".32" d="M0 24h64v40H0z"/>
-                                            <path fill="url(#feature-3-b)" d="M40 24H24L0 64h64z"/>
-                                            <path d="M10 10v22c0 12.15 9.85 22 22 22h22V32c0-12.15-9.85-22-22-22H10z" fill="url(#feature-3-c)" transform="rotate(45 32 32)"/>
-                                        </g>
-                                    </svg>
+                                <div className="row-icon">
+                                    <div class="feature-icon">
+                                        <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
+                                            <defs>
+                                                <linearGradient x1="50%" y1="100%" x2="50%" y2="43.901%" id="feature-3-a">
+                                                    <stop stop-color="rgb(41, 110, 229)" stop-opacity=".798" offset="0%"/>
+                                                    <stop stop-color="rgb(41, 110, 229)" stop-opacity="0" offset="100%"/>
+                                                </linearGradient>
+                                                <linearGradient x1="58.893%" y1="100%" x2="58.893%" y2="18.531%" id="feature-3-b">
+                                                    <stop stop-color="rgb(41, 110, 229)" stop-opacity=".8" offset="0%"/>
+                                                    <stop stop-color="rgb(41, 110, 229)" stop-opacity="0" offset="100%"/>
+                                                </linearGradient>
+                                                <linearGradient x1="50%" y1="100%" x2="50%" y2="0%" id="feature-3-c">
+                                                    <stop stop-color="#b79a64" offset="0%"/>
+                                                    <stop stop-color="#ffbf0d" stop-opacity=".798" offset="49.935%"/>
+                                                    <stop stop-color="#b79a64" stop-opacity="0" offset="100%"/>
+                                                </linearGradient>
+                                            </defs>
+                                            <g fill="none" fill-rule="evenodd">
+                                                <path fill="url(#feature-3-a)" opacity=".32" d="M0 24h64v40H0z"/>
+                                                <path fill="url(#feature-3-b)" d="M40 24H24L0 64h64z"/>
+                                                <path d="M10 10v22c0 12.15 9.85 22 22 22h22V32c0-12.15-9.85-22-22-22H10z" fill="url(#feature-3-c)" transform="rotate(45 32 32)"/>
+                                            </g>
+                                        </svg>
+                                    </div>
+                                    <h3 class="feature-title mt-24">Objective</h3>
                                 </div>
-                                <h3 class="feature-title mt-24">Objective</h3>
                                 <p class="text-sm mb-0">
                                     The ultimate objective is 
                                     to improve adolescent health outcomes by providing
