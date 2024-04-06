62d0addadabbf58f5771b0f1646f918a6607a94a
dodziraynard <dodzireynard@gmail.com>
2024-04-06 15:19:23 +0000

    show remote sync status

--- web/src/widgets/TreatmentsWidget/index.jsx
+++ web/src/widgets/TreatmentsWidget/index.jsx
@@ -63,9 +63,9 @@ function TreatmentsWidget() {
                                                         onClick={() => null}>
                                                         <i className="bi bi-list me-1"></i> More
                                                     </Link>
-                                                    {!item?.synced ?
-                                                        <i className="bi bi-cloud-check mx-2 text-success" title='Synced with remote server'></i> :
-                                                        <i className="bi bi-cloud-slash mx-2 text-danger" title='Synced with remote server'></i>
+                                                    {item?.localnode?.toLowerCase() != "live" && !item?.synced ?
+                                                        <i className="bi bi-cloud-slash mx-2 text-danger" title='Synced with remote server'></i> :
+                                                        <i className="bi bi-cloud-check mx-2 text-success" title='Synced with remote server'></i>
                                                     }
                                                 </div>
                                             )
