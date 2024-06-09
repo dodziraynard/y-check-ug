2a5ddbbfcc82d0ad041eff7129be99351076dca4
dodziraynard <dodzireynard@gmail.com>
2024-06-09 09:43:48 +0100

    Update action status resolution

--- web/src/widgets/ProcessReviewWidget/index.jsx
+++ web/src/widgets/ProcessReviewWidget/index.jsx
@@ -8,6 +8,8 @@ import useAxios from '../../app/hooks/useAxios';
 import SummaryFlagLegend from '../../components/SummaryFlagLegend';
 import { resourceApiSlice } from '../../features/resources/resources-api-slice';
 import { usePutUpdateAdolescentStatusMutation } from '../../features/resources/resources-api-slice';
+
+
 function ProcessReviewWidget() {
   const { pid } = useParams();
   const toast = useToast();
@@ -19,13 +21,15 @@ function ProcessReviewWidget() {
   const [referrals, setReferrals] = useState([])
   const [updateAdolescentStatus, { isLoading: isUpdatingAdolescent, error: errorUpdatingAdolescent }] = usePutUpdateAdolescentStatusMutation()
 
+  const [isAllGood, setIsAllGood] = useState(true)
+
   useEffect(() => {
     getAdolescentProfile();
     getAdolescentFlag();
     getReferrals({ pid })
   }, []);
 
-  
+
   useEffect(() => {
     if (Boolean(adolescentResponseData?.adolescent)) {
       setAdolescent(adolescentResponseData.adolescent);
@@ -34,13 +38,25 @@ function ProcessReviewWidget() {
 
   useEffect(() => {
     if (adolescentFlagResponseData && Array.isArray(adolescentFlagResponseData.flags)) {
-        setAdolescentFlag(adolescentFlagResponseData.flags);
+      setAdolescentFlag(adolescentFlagResponseData.flags);
+
+      adolescentFlagResponseData.flags?.forEach(flag => {
+        if (flag.status === "pending") {
+          setIsAllGood(false)
+        }
+      })
     }
   }, [adolescentFlagResponseData]);
 
   useEffect(() => {
     if (referralsResponse && Array.isArray(referralsResponse.referrals)) {
-        setReferrals(referralsResponse.referrals);
+      setReferrals(referralsResponse.referrals);
+
+      referralsResponse.referrals?.forEach(referral => {
+        if (referral.is_onsite && referral.status?.toLowerCase() !== "completed") {
+          setIsAllGood(false)
+        }
+      })
     }
   }, [referralsResponse]);
 
@@ -61,58 +77,55 @@ function ProcessReviewWidget() {
 
   useEffect(() => {
     if (referralsError) {
-        toast({
-            position: 'top-center',
-            title: `An error occurred: ${referralsError.originalStatus}`,
-            description: referralsError.status,
-            status: 'error',
-            duration: 2000,
-            isClosable: true,
-        })
+      toast({
+        position: 'top-center',
+        title: `An error occurred: ${referralsError.originalStatus}`,
+        description: referralsError.status,
+        status: 'error',
+        duration: 2000,
+        isClosable: true,
+      })
     }
-    }, [referralsError, toast])
+  }, [referralsError, toast])
 
   useEffect(() => {
     if (errorUpdatingAdolescent) {
-        toast({
-            position: 'top-center',
-            title: `An error occurred: ${errorUpdatingAdolescent.originalStatus}`,
-            description: errorUpdatingAdolescent.status,
-            status: 'error',
-            duration: 2000,
-            isClosable: true,
-        })
+      toast({
+        position: 'top-center',
+        title: `An error occurred: ${errorUpdatingAdolescent.originalStatus}`,
+        description: errorUpdatingAdolescent.status,
+        status: 'error',
+        duration: 2000,
+        isClosable: true,
+      })
     }
-    }, [errorUpdatingAdolescent, toast])
+  }, [errorUpdatingAdolescent, toast])
 
   const getStatusColor = (status) => {
-    return (status === 'completed'  || status === 'referred' || status === 'normal') ? 'green.500' : 'red.500';
+    if (status === 'completed' || status === 'referred' || status === 'normal')
+      return 'green.500'
+    return 'red.500';
   };
 
   const getDisplayStatus = (status) => {
     return getStatusColor(status) === 'red.500' ? 'Must be referred' : status;
   };
 
-  const getDisplayStatusReferral = (status) => {
-    return getStatusColor(status) === 'red.500' ? 'Must be completed' : status;
-  };
-
   const handleUpdateAdolescentStatus = async () => {
     const body = {
-        status: "Completed",
+      status: "completed",
     }
-    console.log(body)
     try {
-        const response = await updateAdolescentStatus({ body, pid }).unwrap()
-        if (response['error_message'] != null) {
-          toast({
-              position: 'top-center',
-              title: `An error occurred`,
-              description: response["error_message"],
-              status: 'error',
-              duration: 5000,
-              isClosable: true,
-          })
+      const response = await updateAdolescentStatus({ body, pid }).unwrap()
+      if (response['error_message'] != null) {
+        toast({
+          position: 'top-center',
+          title: `An error occurred`,
+          description: response["error_message"],
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        })
       } else {
         toast({
           position: 'top-center',
@@ -121,20 +134,20 @@ function ProcessReviewWidget() {
           status: 'success',
           duration: 5000,
           isClosable: true,
-      })
-       
+        })
+
       }
-      } catch (err) {
-        toast({
-          position: 'top-center',
-          title: `An error occurred`,
-          description: err.originalStatus,
-          status: 'error',
-          duration: 2000,
-          isClosable: true,
+    } catch (err) {
+      toast({
+        position: 'top-center',
+        title: `An error occurred`,
+        description: err.originalStatus,
+        status: 'error',
+        duration: 2000,
+        isClosable: true,
       })
-      }
-    
+    }
+
   }
   return (
     <Fragment>
@@ -144,9 +157,9 @@ function ProcessReviewWidget() {
         <section className='page-review' style={{ maxWidth: "1024px", margin: "auto" }}>
           <Box as="section" mt={4}>
             <Text color="gray.600">
-              Here is the Summary of the entire process for <strong>{adolescent?.fullname}</strong> ({adolescent?.pid}).
+              Summary of the entire process for <strong>{adolescent?.fullname}</strong> ({adolescent?.pid}).
             </Text>
-            <Heading as="h3" size="sm" mb={4}>Activity Time Section</Heading>
+            <Heading as="h3" size="sm" mb={4}>Activity Time</Heading>
             <Text mt={2}>
               Time spent <strong>(total 3 hours)</strong>
             </Text>
@@ -184,7 +197,7 @@ function ProcessReviewWidget() {
         <section className='page-review mt-5' style={{ maxWidth: "1024px", margin: "auto" }}>
           {/* Referral section  */}
           <Box as="section" mt={8}>
-            <Heading as="h3" size="sm" mb={4}>Referrals Section</Heading>
+            <Heading as="h3" size="sm" mb={4}>Referrals</Heading>
             <TableContainer>
               <Table variant="simple">
                 <Thead>
@@ -194,20 +207,17 @@ function ProcessReviewWidget() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                    {referrals.map((referral, index) => (
-                        <Tr key={index}>
-                        <Td>
-                            {referral.facility_name}
-                            <Text as="span" color="gray.500"> ({referral.services.map(service => service.name).join(', ')})</Text>
-                        </Td>
-                        <Td>
-                            <SummaryFlagLegend colour={referral.color} />
-                        </Td>
-                        <Td color={getStatusColor(referral.status)}>
-                            {getDisplayStatusReferral(referral.status)}
-                        </Td>
-                        </Tr>
-                    ))}                 
+                  {referrals.map((referral, index) => {
+                    return <Tr key={index}>
+                      <Td>
+                        {referral.facility_name}
+                        <Text as="p" color="gray.500"> ({referral.services.map(service => service.name).join(', ')})</Text>
+                      </Td>
+                      <Td color={referral.is_onsite && referral.status?.toLowerCase() !== "completed" ? "red.500" : "green.500"}>
+                        {referral.is_onsite && referral.status?.toLowerCase() !== "completed" ? "Must be referred" : referral.status}
+                      </Td>
+                    </Tr>
+                  })}
                 </Tbody>
               </Table>
             </TableContainer>
@@ -215,52 +225,52 @@ function ProcessReviewWidget() {
         </section>
 
         <section className='page-review mt-5' style={{ maxWidth: "1024px", margin: "auto" }}>
-
           <Box as="section" mt={8}>
-            <Heading as="h3" size="sm" mb={4}>Flags Section</Heading>
+            <Heading as="h3" size="sm" mb={4}>Flags</Heading>
             <TableContainer >
               <Table variant="simple">
                 <Thead>
                   <Tr>
                     <Th borderColor="black">Flags</Th>
-                    <Th borderColor="black">Final Colour</Th>
-                    <Th borderColor="black">Status/Action</Th>
+                    <Th borderColor="black" textAlign={"center"}>Final Colour</Th>
+                    <Th borderColor="black" textAlign={"end"}>Status/Action</Th>
                   </Tr>
                 </Thead>
                 <Tbody>
-                {adolescentFlag.map((flagData, index) => (
+                  {adolescentFlag.map((flagData, index) => (
                     <Tr key={index}>
-                    <Td>{flagData.flag}</Td>
-                    <Td>
+                      <Td>{flagData.flag}</Td>
+                      <Td className='d-flex align-items-center justify-content-center'>
                         <SummaryFlagLegend colour={flagData.color} />
-                    </Td>
-                    <Td color={getStatusColor(flagData.status)}>
+                      </Td>
+                      <Td color={getStatusColor(flagData.status)} textAlign={"end"}>
                         {getDisplayStatus(flagData.status)}
-                    </Td>
+                      </Td>
                     </Tr>
-                ))}
+                  ))}
                 </Tbody>
               </Table>
             </TableContainer>
           </Box >
         </section>
 
-        <section className='mt-5' style={{ maxWidth: "1024px", margin: "auto" }}>
-          <button className='btn btn-danger text-white mx-auto d-block' disabled>All required actions must be completed.</button>
-        </section>
-
-
-        <section className='page-review my-5 d-flex justify-content-center flex-column' style={{ maxWidth: "1024px", margin: "auto" }}>
-          <p className="text text-primary text-center">Congratulation! <strong>{adolescent?.fullname}</strong> have successfully completed the process. </p>
-          <button 
-          className="btn btn-sm btn-primary mx-auto" 
-          type='submit' 
-          disabled={isUpdatingAdolescent} 
-          onClick={handleUpdateAdolescentStatus}>
-          {isUpdatingAdolescent && <Spinner />}
-            Mark as Done
-          </button>
-        </section>
+        {!isAllGood ?
+          <section className='mt-5' style={{ maxWidth: "1024px", margin: "auto" }}>
+            <button className='btn btn-danger text-white mx-auto d-block' disabled>All required actions must be completed.</button>
+          </section>
+          :
+          <section className='page-review my-5 d-flex justify-content-center flex-column' style={{ maxWidth: "1024px", margin: "auto" }}>
+            <p className="text text-primary text-center">Congratulation! <strong>{adolescent?.fullname}</strong> have successfully completed the process. </p>
+            <button
+              className="btn btn-sm btn-primary mx-auto"
+              type='submit'
+              disabled={isUpdatingAdolescent}
+              onClick={handleUpdateAdolescentStatus}>
+              {isUpdatingAdolescent && <Spinner />}
+              Mark as Done
+            </button>
+          </section>
+        }
       </div>
     </Fragment>
   );
